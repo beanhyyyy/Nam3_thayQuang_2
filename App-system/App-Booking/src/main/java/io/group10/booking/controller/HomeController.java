ba4eb8d37be444eb462ccaef8fcb51d4b0e98bce
kanghy <quangnguyenne1411@gmail.com>
2020-06-06 12:15:19 +0700

    commit

--- App-system/App-Booking/src/main/java/io/group10/booking/controller/HomeController.java
+++ App-system/App-Booking/src/main/java/io/group10/booking/controller/HomeController.java
@@ -1,17 +1,28 @@
 package io.group10.booking.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.mail.MessagingException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import io.group10.booking.entity.Booking;
 
-
+@EnableCircuitBreaker
 @RestController
 @RequestMapping("/")
 public class HomeController {
@@ -19,7 +30,23 @@ public class HomeController {
 	private RestTemplate restTemplate;
 	
 	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Autowired
 	private Environment env;
+	
+	public void sendMail(String error) {
+		SimpleMailMessage smg = new SimpleMailMessage();
+		smg.setTo("nguyentrongtambd@gmail.com");
+		smg.setSubject("Error Server");
+		smg.setText(error);
+		
+		System.out.println("Send mail");
+		javaMailSender.send(smg);
+		System.out.println("Send mail successfull");
+	}
+	
+	
 	
 	@RequestMapping("/")
 	public String home() {
@@ -29,6 +56,7 @@ public class HomeController {
 		return "Hello from Booking Service running at port: " + env.getProperty("local.server.port");
 	}
   
+	@HystrixCommand(fallbackMethod="errorGetTickets")
 	@RequestMapping("/ticket/{fim}")
 	public Booking getBooking(@PathVariable String fim) {
 		
@@ -38,6 +66,7 @@ public class HomeController {
 		for (int i = 0; i < hang.length; i++) {
 			String result = restTemplate.getForObject("http://"+hang[i]+"-gateway/"+hang[i]+"/tickets", String.class);	
 			try {
+				@SuppressWarnings("unchecked")
 				Object obj = new org.json.simple.parser.JSONParser().parse(result);
 				org.json.simple.JSONObject ob = (org.json.simple.JSONObject) obj;
 				org.json.simple.JSONArray arr = (org.json.simple.JSONArray) ob.get("ticket");
@@ -79,6 +108,36 @@ public class HomeController {
 		return booking;
 	}
 
+	public Booking errorGetTickets(String firm) throws MessagingException, IOException{
+		Booking tc = new Booking();
+		String[] hang = firm.split(",");
+		JSONArray array = new JSONArray();
+		for (int i = 0; i < hang.length; i++) {
+			JSONParser jsParser = new JSONParser();
+			Object obj;
+			JSONObject jsObj = new JSONObject();
+			try {
+				String result = restTemplate.getForObject("http://"+hang[i]+"-gateway/"+hang[i]+"/tickets", String.class);	
+				obj = jsParser.parse(result);
+			} catch (Exception e) {
+				obj = null;
+			}
+			if(obj == null) {
+				jsObj.put(hang[i],hang[i] + " error");
+				sendMail(hang[i] + " error");
+				array.add(jsObj);
+			}else {
+				JSONObject ob = (JSONObject) obj;
+				JSONArray arr = (JSONArray) ob.get("ticket");
+				JSONObject newObj = new JSONObject();
+				newObj.put(hang[i], arr);
+				array.add(newObj);
+			}
+		}
+		tc.setTicket((List<Object>) array);
+		return tc;
+				
+	}
 	
 	
 	// -------- Admin Area --------
